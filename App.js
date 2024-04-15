f083cad108c2633e8f7672fc39118073cf5d6e61
deep-3110 <deepshah3110@gmail.com>
2024-04-15 11:52:53 -0400

    ServerCommit

--- App.js
+++ App.js
@@ -6,13 +6,43 @@ import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import AssignmentsRoutes from './Kanbas/assignments/routes.js';
 
-
+import mongoose from "mongoose";
+import session from "express-session";
+import "dotenv/config";
+import UserRoutes from './Kanbas/Users/routes.js';
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+console.log(CONNECTION_STRING);
+mongoose.connect(CONNECTION_STRING);
 const app = express();
-app.use(cors());
+app.use(
+    cors({
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    })
+   );
+
+
+   const sessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  };
+
+
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+  }
+  app.use(session(sessionOptions));
+  
 app.use(express.json());
+UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 AssignmentsRoutes(app)
 Lab5(app);
 Hello(app);
-app.listen(process.env.PORT || 4000);
+app.listen(4000);
--- Kanbas/Database/users.js
+++ Kanbas/Database/users.js
@@ -1,92 +1,85 @@
-export default [
-    {
-      _id: "121",
-      username: "iron_man",
-      password: "stark123",
-      firstName: "Tony",
-      lastName: "Stark",
-      email: "tony@stark.com",
-      dob: "1970-05-29",
-      role: "STUDENT",
-    },
-    {
-      _id: "122",
-      username: "dark_knight",
-      password: "wayne123",
-      firstName: "Bruce",
-      lastName: "Wayne",
-      email: "bruce@wayne.com",
-      dob: "1972-02-19",
-      role: "STUDENT",
-    },
-    {
-      _id: "123",
-      username: "capt_america",
-      password: "rogers123",
-      firstName: "Steve",
-      lastName: "Rogers",
-      email: "steve@avengers.com",
-      dob: "1918-07-04",
-      role: "STUDENT",
-    },
-    {
-      _id: "124",
-      username: "black_widow",
-      password: "romanoff123",
-      firstName: "Natasha",
-      lastName: "Romanoff",
-      email: "natasha@avengers.com",
-      dob: "1984-11-22",
-      role: "STUDENT",
-    },
-    {
-      _id: "125",
-      username: "thor_odinson",
-      password: "mjolnir123",
-      firstName: "Thor",
-      lastName: "Odinson",
-      email: "thor@asgard.com",
-      dob: "982-05-25",
-      role: "STUDENT",
-    },
-    {
-      _id: "126",
-      username: "hulk_smash",
-      password: "banner123",
-      firstName: "Bruce",
-      lastName: "Banner",
-      email: "bruce@avengers.com",
-      dob: "1969-12-18",
-      role: "STUDENT",
-    },
-    {
-      _id: "127",
-      username: "ring_bearer",
-      password: "shire123",
-      firstName: "Frodo",
-      lastName: "Baggins",
-      email: "frodo@shire.com",
-      dob: "1368-09-22",
-      role: "FACULTY",
-    },
-    {
-      _id: "128",
-      username: "strider",
-      password: "aragorn123",
-      firstName: "Aragorn",
-      lastName: "Elessar",
-      email: "aragorn@gondor.com",
-      dob: "2931-03-01",
-      role: "FACULTY",
-    },
-    {
-      _id: "129",
-      username: "elf_archer",
-      password: "legolas123",
-      firstName: "Legolas",
-      lastName: "Greenleaf",
-      email: "legolas@mirkwood.com",
-      dob: "2879-07-15",
-      role: "FACULTY",
-    },
-  ];
+export default[{
+  "_id": {
+    "$oid": "661864fd4c83e5c123854d43"
+  },
+  "username": "deepshah@3110",
+  "password": "IamSmart",
+  "role": "USER",
+  "__v": 0
+},
+{
+  "_id": {
+    "$oid": "661865784c83e5c123854d46"
+  },
+  "username": "demo@123",
+  "password": "Demo!23",
+  "role": "USER",
+  "__v": 0
+},
+{
+  "_id": {
+    "$oid": "661865884c83e5c123854d49"
+  },
+  "username": "deep@31",
+  "password": "deepshah",
+  "role": "USER",
+  "__v": 0
+},
+{
+  "_id": {
+    "$oid": "661c8478191a3c96cabf6a85"
+  },
+  "username": "ddddeeeeeeeeeeeeee",
+  "password": "dedde",
+  "role": "USER",
+  "__v": 0
+},
+{
+  "_id": {
+    "$oid": "661d3d5f954ea42dd94f1d19"
+  },
+  "username": "iron_mansss",
+  "password": "stark123",
+  "role": "FACULTY",
+  "__v": 0,
+  "email": "ddddddddd",
+  "firstName": "dddd",
+  "lastName": "dddddccccccccc"
+},
+{
+  "_id": {
+    "$oid": "661d446e8881ade098f8ecf8"
+  },
+  "username": "demo123",
+  "password": "demo123",
+  "role": "USER",
+  "__v": 0
+},
+{
+  "_id": {
+    "$oid": "661d44bb8881ade098f8ecfd"
+  },
+  "username": "Test123",
+  "password": "Test123",
+  "role": "USER",
+  "__v": 0,
+  "email": "ddddddd",
+  "firstName": "dddddddddd",
+  "lastName": "dddddd"
+},
+{
+  "_id": {
+    "$oid": "661d44bb8881ade091f8ecfd"
+  },
+  "username": "hulk_smashs-no",
+  "password": "banner123s",
+  "firstName": "Bruce",
+  "lastName": "Banners",
+  "email": "bruce@avengers.com",
+  "dob": {
+    "$date": {
+      "$numberLong": "-1209600000"
+    }
+  },
+  "role": "STUDENT"
+}];
